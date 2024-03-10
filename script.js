d31e33646e0cf9d6a891cdf9017e3d9ff9d5ecbf
Stanislaw <36824039+StasKrivoscheenko@users.noreply.github.com>
2024-03-10 20:05:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menu = document.getElementById("admin-info");
+    menu.classList.toggle("show");
+}
+function toggleMenu2() {
+    var menu = document.getElementById("front-info");
+    menu.classList.toggle("show");
+}
